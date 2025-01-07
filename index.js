8a38737fbd70c2b0e398ef5c30bc6befb7dc1671
gneval9 <142158863+gneval9@users.noreply.github.com>
2025-01-07 17:45:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function send() {
+    var name = document.getElementById("text1").value;
+    var lastname = document.getElementById("text2").value;
+
+
+    if (name == "" || lastname == "") {
+        alert("Los campos no pueden estar vacios");
+        return;
+        }
+
+    if (name == "Arturo" && lastname == "Galiana") {
+        alert("Hola " + name + " " + lastname + ", seras redirigido a la pagina en breve");
+        window.location.href = "final.html";
+        }
+
+    else {
+        alert("No puedes acceder a esta pagina");
+        return;
+    }
+}
